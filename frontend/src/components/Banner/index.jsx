a7ce9034c113da64de733a5cdba4bdb54966538b
Aaron <aarondivi@gmail.com>
2023-05-04 12:44:24 +0200

    init

---
+++ frontend/src/components/Banner/index.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom'
+import Video from '../Video'
+import me from '../../assets/pictures/me.jpg'
+import me2 from '../../assets/pictures/me2.jpg'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// styles
+import './m-banner.css'
+import './d-banner.css'
+
+const Banner = () => {
+
+	return (
+		<section id='banner' className='banner'>
+			<h1 className='banner__title'>
+				<span className='banner__title-v'>V</span>
+				<span className='banner__title-o'>O</span>
+				tre
+				<br/>
+				<span className='banner__title-w'>W</span>
+				eb
+				<br/>
+				<span className='banner__title-d'>D</span>
+				ev.
+			</h1>
+			<h2 className='banner__subtitle'>Simplement votre !</h2>
+			<p className='banner__text'>
+				Aaron DIVIOKA - <span><strong>(+33) 635 774 835</strong></span>
+				<br/>
+				<a href='mailto:contact@vowd.fr'>contact@vowd.fr</a> - www.vowd.fr
+				<br/>
+				Consultant en développement web
+				<br/> 
+				& communication digitale
+				<br/>
+				<strong className='banner__text__end'>
+					Pour les entrepreneurs ambitieux
+				</strong>
+			</p>
+			<Link  className='banner__button' to=''>
+				<FontAwesomeIcon 
+					className='banner__button__btn' 
+					icon={faCirclePlay} 
+				/>
+				<p className='banner__button__text'>
+					Tout savoir
+					<br/>
+					en 5 minutes
+				</p>
+			</Link>
+			<img className='banner__picture' src={me} alt='congratulation' />
+			<img className='banner__picture2' src={me2} alt='congratulation' />
+			<Video />
+		</section>
+	)
+}
+ 
+export default Banner
